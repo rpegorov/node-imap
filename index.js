07c6291f768f70c051be667fed68536c40414edd
Rostislav Egorov <rpegorov@icloud.com>
2023-06-23 16:26:36 +0300

    получение почты, поиск вложений и скачивание вложений с декодировокой

--- index.js
+++ index.js
@@ -1,65 +1,112 @@
-const Imap = require('node-imap');
-const inspect = require('util').inspect;
+const config = require('./config.json');
 
-const imap = new Imap({
-    user: 'example@yandex.ru',
-    password: 'password',
-    host: 'imap.yandex.ru',
-    port: 993,
-    tls: true
-});
+const fs = require('fs');
+const { Base64Decode } = require('base64-stream')
+const libqp = require('libqp');
 
-function openInbox(cb) {
-    imap.openBox('INBOX', false, cb);
+const Imap = require('imap');
+const imap = new Imap(config.imap);
+
+function findAttachmentParts(struct, attachments) {
+  attachments = attachments ||  [];
+  for (var i = 0, len = struct.length, r; i < len; ++i) {
+    if (Array.isArray(struct[i])) {
+      findAttachmentParts(struct[i], attachments);
+    } else {
+      if (struct[i].disposition && ['inline', 'attachment'].indexOf(struct[i].disposition.type.toLowerCase()) > -1) {
+        attachments.push(struct[i]);
+      }
+    }
+  }
+  return attachments;
 }
 
-imap.once('ready', () => {
-    openInbox((err, box) => {
-        if (err) throw err;
-        imap.search(['UNSEEN'], (err, results) => {
-            if (err) throw err;
-            console.log(`Найдено ${results.length} непрочитанных сообщений`);
-            const fetchOptions = {
-                bodies: ['HEADER', 'TEXT'],
-                markSeen: true
-            };
-            const f = imap.fetch(results, fetchOptions);
-            f.on('message', (msg, seqno) => {
-                console.log(`Сообщение #${seqno}`);
-                const prefix = `Сообщение #${seqno}: `;
-                msg.on('body', (stream, info) => {
-                    if (info.which === 'HEADER') {
-                        let buffer = '';
-                        stream.on('data', (chunk) => {
-                            buffer += chunk.toString('utf8');
-                        });
-                        stream.once('end', () => {
-                            const headers = Imap.parseHeader(buffer);
-                            console.log(`${prefix}От: ${headers.from}`);
-                            console.log(`${prefix}Тема: ${headers.subject}`);
-                        });
-                    }
-                });
-                msg.once('end', () => {
-                    console.log(`${prefix}Завершено`);
-                });
-            });
-            f.once('error', (err) => {
-                console.log(`Ошибка получения сообщений: ${err}`);
-            });
-            f.once('end', () => {
-                console.log('Завершено');
-                imap.end();
-            });
-        });
+function buildAttMessageFunction(attachment) {
+  let filename = attachment.params.name;
+  let encoding = attachment.encoding;
+  let decoder = new libqp.Decoder();
+  
+
+  return function (msg, seqno) {
+    let prefix = '(#' + seqno + ') ';
+    msg.on('body', function(stream, info) {
+      //Create a write stream so that we can stream the attachment to file;
+      console.log(prefix + 'Streaming this attachment to file', filename, info);
+      let writeStream = fs.createWriteStream( (config.downloads && config.downloads.directory) ? `${config.downloads.directory}/${filename}` : filename);
+      writeStream.on('finish', function() {
+        console.log(prefix + 'Done writing to file %s', filename);
+      });
+
+      //so we decode during streaming using 
+      // if (encoding.toLowerCase() === 'base64') {
+        //the stream is base64 encoded, so here the stream is decode on the fly and piped to the write stream (file)
+        stream.pipe(decoder).pipe(writeStream)
+      // } else  {
+        //here we have none or some other decoding streamed directly to the file which renders it useless probably
+        // stream.pipe(writeStream);
+      // }
     });
+    msg.once('end', function() {
+      console.log(prefix + 'Finished attachment %s', filename);
+    });
+  };
+}
+
+imap.once('ready', function() {
+  imap.openBox('INBOX', true, function(err, box) {
+    if (err) throw err;
+    var f = imap.fetch('1:*', {
+      bodies: ['', 'TEXT'],
+      struct: true
+    });
+    f.on('message', function (msg, seqno) {
+      console.log('Message #%d', seqno);
+      const prefix = '(#' + seqno + ') ';
+      msg.on('body', function(stream, info) {
+        var buffer = '';
+        stream.on('data', function(chunk) {
+          buffer += chunk.toString('utf8');
+        });
+        stream.once('end', function() {
+          console.log(prefix + 'Parsed header: %s', Imap.parseHeader(buffer));
+          console.log(Imap.parseHeader(buffer).from)
+          console.log(Imap.parseHeader(buffer).subject)
+        });
+      });
+      msg.once('attributes', function(attrs) {
+        const attachments = findAttachmentParts(attrs.struct);
+        console.log(prefix + 'Has attachments: %d', attachments.length);
+        for (var i = 0, len=attachments.length ; i < len; ++i) {
+          const attachment = attachments[i];
+          console.log(prefix + 'Fetching attachment %s', attachment.params.name);
+          var f = imap.fetch(attrs.uid , {
+            bodies: [attachment.partID],
+            struct: true
+          });
+          //build function to process attachment message
+          f.on('message', buildAttMessageFunction(attachment));
+        }
+      });
+      msg.once('end', function() {
+        console.log(prefix + 'Finished email');
+      });
+    });
+    f.once('error', function(err) {
+      console.log('Fetch error: ' + err);
+    });
+    f.once('end', function() {
+      console.log('Done fetching all messages!');
+      imap.end();
+    });
+  });
 });
 
-imap.once('error', (err) => {
-    console.log(`Ошибка IMAP: ${err}`);
+imap.once('error', function(err) {
+  console.log(err);
 });
 
-imap.once('end', () => {
-    console.log('IMAP соединение завершено');
+imap.once('end', function() {
+  console.log('Connection ended');
 });
+
 imap.connect();
