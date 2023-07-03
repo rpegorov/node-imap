3f28459e7fb8ba7e62a244a2d9027b1d9551f1ce
Rostislav Egorov <rpegorov@icloud.com>
2023-07-03 16:39:38 +0300

    delete const

--- src/connections/ImapConnection.ts
+++ src/connections/ImapConnection.ts
@@ -5,7 +5,6 @@ import Imap = require('node-imap');
 export class ImapConnections {
 
     static create(imap: IImap) {
-        const Imap = require('node-imap');
         return new Imap({
             user: imap.user,
             password: imap.password,
