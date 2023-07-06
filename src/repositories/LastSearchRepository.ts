058654985dda18c19623d24e771c708a670be612
Rostislav Egorov <rpegorov@icloud.com>
2023-07-06 12:55:06 +0300

    добавлена проверка по дате, рефакторинг репоса

--- src/repositories/LastSearchRepository.ts
+++ src/repositories/LastSearchRepository.ts
@@ -4,15 +4,26 @@ import { AppDataSource } from "../connections/SqlLiteConnection";
 import { LastSearchData } from "../entities/LastSearchData";
 
 export class LastSearchRepository {
-    static save = async (lastSearchData: ISearchData): Promise<InsertResult> =>
-        AppDataSource.getRepository(LastSearchData).upsert(lastSearchData, ['user']);
+    static async save(lastSearchData: ISearchData): Promise<InsertResult> {
+        return await AppDataSource.getRepository(LastSearchData).upsert(lastSearchData, ['user']);
+    }
 
-    static findLastMessageId = async (user: string): Promise<number> => {
+    static async findLastMessageId(user: string): Promise<number> {
         const lastSearchData = await AppDataSource.getRepository(LastSearchData).findOne({
             where: { user },
             order: { dateLastMessage: 'DESC' },
             select: ['lastMessageId'],
         });
         return lastSearchData?.lastMessageId ?? 1;
+    };
+
+    static async dateLastMessage(user: string): Promise<any> {
+        const dateLastMessage = await AppDataSource.getRepository(LastSearchData).findOne({
+            where: { user },
+            order: { dateLastMessage: 'DESC' },
+            select: ['dateLastMessage'],
+        });
+        const today = new Date();
+        return dateLastMessage?.dateLastMessage ?? today.toLocaleString();
     };
 }
--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -7,14 +7,14 @@ import Connection, { ImapMessage, ImapMessageAttributes, ImapMessageBodyInfo } f
 import { Stream } from "stream";
 
 export class SearchService {
-  static searchAndRetrieveAttachments(imap: IImap): Promise<IAttachment[]> {
+  searchAndRetrieveAttachments(imap: IImap): Promise<IAttachment[]> {
     const connection = ImapConnections.create(imap);
 
     return new Promise<IAttachment[]>((resolve, reject) => {
       connection.once('ready', async () => {
         try {
-          const foundMessage = await searchMessages(connection);
-          const attachments = await fetchAttachmentParts(connection, foundMessage);
+          const foundMessage = await this.searchMessages(connection, imap);
+          const attachments = await this.fetchAttachmentParts(connection, foundMessage, imap);
           resolve(attachments);
         } catch (err) {
           reject(err);
@@ -24,139 +24,146 @@ export class SearchService {
       });
       connection.connect();
     });
+  }
 
-    async function searchMessages(connection: Connection): Promise<number[]> {
-      const lastSearchData = await LastSearchRepository.findLastMessageId(imap.user);
-      const regex = /резюме/igm;
-      const uidCriteria = ['UID', lastSearchData + 1 + ':*'];
-      const textCriteria = ['TEXT', regex.source];
-
-      return new Promise((resolve, reject) => {
-        connection.openBox('INBOX', (error: Error, mailboxStatus: any) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-
-          const lastMessageId = mailboxStatus.uidnext - 1;
-
-          if (lastSearchData >= lastMessageId) {
-            reject(error);
-            return;
-          }
-
-          connection.search(
-            [uidCriteria, textCriteria],
-            (error: Error, foundMessage: number[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(foundMessage);
-            }
-          );
-        });
-      });
+  async searchMessages(connection: Connection, imap: IImap): Promise<number[]> {
+    let lastSearchData = await LastSearchRepository.findLastMessageId(imap.user);
+    if (lastSearchData != 1) {
+      lastSearchData = lastSearchData + 1;
     }
+    const dateLastMessage = await LastSearchRepository.dateLastMessage(imap.user);
+    const regex = /резюме/igm;
+    const uidCriteria = ['UID', lastSearchData + ':*'];
+    const textCriteria = ['TEXT', regex.source];
+    const sienceCriteria = ['SINCE', dateLastMessage]
 
-    function fetchAttachmentParts(connection: Connection, foundMessage: number[]): Promise<IAttachment[]> {
-      const fetch = connection.fetch(foundMessage, {
-        bodies: ['', 'TEXT'],
-        struct: true
-      });
+    return new Promise((resolve, reject) => {
+      connection.openBox('INBOX', (error: Error, mailboxStatus: any) => {
+        if (error) {
+          reject(error);
+          return;
+        }
 
-      const attachmentPromises: Promise<IAttachment>[] = [];
+        const lastMessageId = mailboxStatus.uidnext - 1;
 
-      fetch.on('message', (msg: ImapMessage) => {
-        msg.once('attributes', (attrs: ImapMessageAttributes) => {
-          try {
-            const attachments = findAttachmentParts(attrs.struct);
-            for (let i = 0, len = attachments.length; i < len; ++i) {
-              const attachment = attachments[i];
-              let attachmentPromise = new Promise<IAttachment>((resolve, reject) => {
-                let fetchParts = connection.fetch(attrs.uid, {
-                  bodies: [attachment.partID],
-                  struct: true
-                });
-                fetchParts.on('message', retrieveAttachments(attachment, resolve, reject));
-              });
+        if (lastSearchData >= lastMessageId) {
+          reject(error);
+          return;
+        }
 
-              attachmentPromises.push(attachmentPromise);
-
-              const lastSearchData = new LastSearchData();
-              lastSearchData.dateLastMessage = attrs.date;
-              lastSearchData.lastMessageId = attrs.uid;
-              lastSearchData.user = imap.user;
-              LastSearchRepository.save(lastSearchData);
+        connection.search(
+          [uidCriteria, sienceCriteria, textCriteria],
+          (error: Error, foundMessage: number[]) => {
+            if (error) {
+              reject(error);
+              return;
             }
-          } catch (err) {
-            console.error('Error fetching attachment parts:', err);
+            resolve(foundMessage);
           }
-        });
+        );
       });
+    });
+  }
 
-      return new Promise<IAttachment[]>((resolve, reject) => {
-        fetch.once('error', (err: Error) => {
+  fetchAttachmentParts(connection: Connection, foundMessage: number[], imap: IImap): Promise<IAttachment[]> {
+    const fetch = connection.fetch(foundMessage, {
+      bodies: ['', 'TEXT'],
+      struct: true
+    });
+
+    const attachmentPromises: Promise<IAttachment>[] = [];
+
+    fetch.on('message', (msg: ImapMessage) => {
+      msg.once('attributes', (attrs: ImapMessageAttributes) => {
+        try {
+          const attachments = this.findAttachmentParts(attrs.struct);
+          for (let i = 0, len = attachments.length; i < len; ++i) {
+            const attachment = attachments[i];
+            let attachmentPromise = new Promise<IAttachment>((resolve, reject) => {
+              let fetchParts = connection.fetch(attrs.uid, {
+                bodies: [attachment.partID],
+                struct: true
+              });
+              fetchParts.on('message', this.retrieveAttachments(attachment, resolve, reject));
+            });
+            attachmentPromises.push(attachmentPromise);
+          }
+        } catch (err) {
+          console.error('Error fetching attachment parts:', err);
+        }
+        this.saveResultData(attrs, imap);
+      });
+    });
+
+    return new Promise<IAttachment[]>((resolve, reject) => {
+      fetch.once('error', (err: Error) => {
+        reject(err);
+      });
+      fetch.once('end', async () => {
+        try {
+          const attachments = await Promise.all(attachmentPromises);
+          resolve(attachments);
+          connection.end();
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  retrieveAttachments(attachment: any, resolve: any, reject: any) {
+    let filename = attachment.params.name;
+
+    return (msg: ImapMessage, seqno: number) => {
+      let prefix = '(#' + seqno + ') ';
+      let chunks: Buffer[] = [];
+
+      msg.on('body', function (stream: Stream, info: ImapMessageBodyInfo) {
+        console.log(prefix + 'Streaming this attachment:', filename, info);
+
+        stream.on('data', function (chunk: Buffer) {
+          chunks.push(chunk);
+        });
+
+        stream.on('end', function () {
+          let content = Buffer.concat(chunks);
+
+          let attachmentData: IAttachment = {
+            filename: filename,
+            subType: attachment.subtype,
+            content: content.toString(),
+            type: attachment.type
+          };
+          resolve(attachmentData);
+        });
+
+        stream.on('error', function (err: Error) {
+          console.error(prefix + 'Error streaming attachment:', filename, err);
           reject(err);
         });
-        fetch.once('end', async () => {
-          try {
-            const attachments = await Promise.all(attachmentPromises);
-            resolve(attachments);
-            connection.end();
-          } catch (err) {
-            reject(err);
-          }
-        });
       });
-    }
+    };
+  }
 
-    function retrieveAttachments(attachment: any, resolve: any, reject: any) {
-      let filename = attachment.params.name;
-
-      return (msg: ImapMessage, seqno: number) => {
-        let prefix = '(#' + seqno + ') ';
-        let chunks: Buffer[] = [];
-
-        msg.on('body', function (stream: Stream, info: ImapMessageBodyInfo) {
-          console.log(prefix + 'Streaming this attachment:', filename, info);
-
-          stream.on('data', function (chunk: Buffer) {
-            chunks.push(chunk);
-          });
-
-          stream.on('end', function () {
-            let content = Buffer.concat(chunks);
-
-            let attachmentData: IAttachment = {
-              filename: filename,
-              subType: attachment.subtype,
-              content: content.toString(),
-              type: attachment.type
-            };
-            resolve(attachmentData);
-          });
-
-          stream.on('error', function (err: Error) {
-            console.error(prefix + 'Error streaming attachment:', filename, err);
-            reject(err);
-          });
-        });
-      };
-    }
-
-    function findAttachmentParts(struct: any, attachments: any[] = []): any[] {
-      attachments = attachments || [];
-      for (let i = 0, len = struct.length, r; i < len; ++i) {
-        if (Array.isArray(struct[i])) {
-          findAttachmentParts(struct[i], attachments);
-        } else {
-          if (struct[i].disposition && ['inline', 'attachment'].indexOf(struct[i].disposition.type.toLowerCase()) > -1) {
-            attachments.push(struct[i]);
-          }
+  findAttachmentParts(struct: any, attachments: any[] = []): any[] {
+    attachments = attachments || [];
+    for (let i = 0, len = struct.length, r; i < len; ++i) {
+      if (Array.isArray(struct[i])) {
+        this.findAttachmentParts(struct[i], attachments);
+      } else {
+        if (struct[i].disposition && ['inline', 'attachment'].indexOf(struct[i].disposition.type.toLowerCase()) > -1) {
+          attachments.push(struct[i]);
         }
       }
-      return attachments;
     }
+    return attachments;
+  }
+
+  saveResultData(attrs: ImapMessageAttributes, imap: IImap): any {
+    const lastSearchData = new LastSearchData();
+    lastSearchData.dateLastMessage = attrs.date;
+    lastSearchData.lastMessageId = attrs.uid;
+    lastSearchData.user = imap.user;
+    LastSearchRepository.save(lastSearchData);
   }
 }
